d2215067ed04bb5a0c4e714679f21fa796b972fa
verycoolmathgames <52218872+verycoolmathgames@users.noreply.github.com>
2019-07-03 17:08:40 -0400

    stuff

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
 
 ReactDOM.render(<App />, document.getElementById('root'))
+
+var admin = require('firebase-admin');
+
+admin.initializeApp({
+    credential: admin.credential.applicationDefault(),
+    databaseURL: 'https://<DATABASE_NAME>.firebaseio.com'
+});
