2647b1754ca8147c813b429dcc7c29b3b5d18fb1
verycoolmathgames <52218872+verycoolmathgames@users.noreply.github.com>
2019-07-03 18:13:58 -0400

    stuff

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
+// eslint-disable-next-line
 import * as firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
@@ -8,4 +9,5 @@ import "firebase/firestore";
 ReactDOM.render(<App />, document.getElementById('root'))
 
 var admin = require('firebase-admin');
+// eslint-disable-next-line
 var app = admin.initializeApp();
