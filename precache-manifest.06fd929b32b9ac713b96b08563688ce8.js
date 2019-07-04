d5854a5a0e340aecb5c2e129d944abf6db1b16d1
verycoolmathgames <52218872+verycoolmathgames@users.noreply.github.com>
2019-07-03 22:41:03 -0400

    Updates

---
+++ precache-manifest.06fd929b32b9ac713b96b08563688ce8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "131087f3cf3aa5b50f29f4d651b8fa9f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5637fead98b0e3db67b",
+    "url": "/static/css/main.6e97b402.chunk.css"
+  },
+  {
+    "revision": "d75523816dcde6e32483",
+    "url": "/static/js/2.43ed196f.chunk.js"
+  },
+  {
+    "revision": "e5637fead98b0e3db67b",
+    "url": "/static/js/main.a1d35940.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
