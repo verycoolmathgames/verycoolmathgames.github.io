18ba2282c4d69b8425f7d89e1b68c272a554da36
verycoolmathgames <52218872+verycoolmathgames@users.noreply.github.com>
2019-07-04 11:03:15 -0400

    Revert "stuff"
    This reverts commit 2647b1754ca8147c813b429dcc7c29b3b5d18fb1.

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Typography, Paper, Avatar, Button } from '@material-ui/core'
+import React, { useEffect, useState } from 'react'
+import { Typography, Paper, Avatar, CircularProgress, Button } from '@material-ui/core'
 import VerifiedUserOutlined from '@material-ui/icons/VerifiedUserOutlined'
 import withStyles from '@material-ui/core/styles/withStyles'
 import firebase from '../firebase'
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
-// eslint-disable-next-line
 import * as firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
@@ -9,5 +8,4 @@ import "firebase/firestore";
 ReactDOM.render(<App />, document.getElementById('root'))
 
 var admin = require('firebase-admin');
-// eslint-disable-next-line
 var app = admin.initializeApp();
