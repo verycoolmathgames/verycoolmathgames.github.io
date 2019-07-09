ff1f817308c61e6eef89e692b801c4d0b7faa28c
verycoolmathgames <52218872+verycoolmathgames@users.noreply.github.com>
2019-07-09 17:12:40 -0400

    Gitacora saved 1 changed files

---
+++ public/chatroom/app.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", event => {
+
+    const app = firebase.app();
+    const db = firebase.firestore();
+    const chatHistory = db.collection("website_chat").doc("5rz9qkx3yCvAzEuPiiOt")
+
+    chatHistory.onSnapshot(doc => {
+        const chatData = doc.data();
+        document.write(chatData.displayName + `<br>`)
+        document.write(chatData.text + `<br>`)
+    })
+
+});
