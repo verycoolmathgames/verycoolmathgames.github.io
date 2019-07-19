b252bcde6d7fee5aa6c1ad110efa82861d80c6fa
verycoolmathgames <52218872+verycoolmathgames@users.noreply.github.com>
2019-07-18 21:14:15 -0400

    Gitacora saved 1 changed files

--- public/main/main.js
+++ public/main/main.js
@@ -5,6 +5,16 @@ import { initializeApp, firestore, auth } from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 
+initializeApp({
+    apiKey: "AIzaSyCxRTkWjoToUoNsM8Rm6zPwiJBG_JCB4fo",
+    authDomain: "verycoolthanksforsharing.firebaseapp.com",
+    databaseURL: "https://verycoolthanksforsharing.firebaseio.com",
+    projectId: "verycoolthanksforsharing",
+    storageBucket: "verycoolthanksforsharing.appspot.com",
+    messagingSenderId: "78626384450",
+    appId: "1:78626384450:web:5c1a5470485502ab"
+});
+
 var db = firestore();
 
 function signIn(email, password, ep) {
