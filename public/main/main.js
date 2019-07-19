101d53880cb267ed83fa4d8e8b1f0d703f40bc58
verycoolmathgames <52218872+verycoolmathgames@users.noreply.github.com>
2019-07-18 20:57:56 -0400

    Gitacora saved 1 changed files

--- public/main/main.js
+++ public/main/main.js
@@ -1,3 +1,9 @@
+var firebase = require("firebase/app");
+
+// Add the Firebase products that you want to use
+require("firebase/auth");
+require("firebase/firestore");
+
 firebase.initializeApp({
     apiKey: "AIzaSyCxRTkWjoToUoNsM8Rm6zPwiJBG_JCB4fo",
     authDomain: "verycoolthanksforsharing.firebaseapp.com",
