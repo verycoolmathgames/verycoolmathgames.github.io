f57aec3b5f6b39b24c079ab6768318def588792b
verycoolmathgames <52218872+verycoolmathgames@users.noreply.github.com>
2019-07-19 11:35:58 -0400

    Gitacora saved 1 changed files

--- public/main/main.js
+++ public/main/main.js
@@ -41,7 +41,7 @@ function email() {
 
     var users = db.collection("users");
     users.doc(uid).get().then(function (doc) {
-        var displayName = doc.data().displayName
+        var displayName = doc.data().displayName;
         return email;
     });
 };
