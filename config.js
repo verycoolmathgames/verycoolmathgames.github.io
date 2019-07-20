3c17421f66a01a0c83b3fd0ba7ce84e3dadaef67
RoyceA-cyber <52252050+RoyceA-cyber@users.noreply.github.com>
2019-07-20 13:08:26 -0400

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-var chConfig = {
-    masterServer: "/master/api.php",
-    serverName: "CELL.SH",
-    fbAppId: 325763913847,
-    googleAppId: '325763913847-tidhg175e0gs4nj29g8nbq5mm1jj2f35.apps.googleusercontent.com',
-    recaptchaAppId: '6LdAeg4UAAAAAP6IyCmH-dk4tYJGLTPmJrlMe7NJ',
-	analyticsAppId: 'UA-88753192-1'
-};
