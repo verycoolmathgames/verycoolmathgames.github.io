35e98e0be47fa9299f9cf8743cafa16b4f31de93
verycoolmathgames <52218872+verycoolmathgames@users.noreply.github.com>
2019-07-26 14:53:00 -0400

    stuff

---
+++ public/main/points.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+
+import * as firebase from "firebase/app";
+
+import "firebase/auth";
+import "firebase/firestore";
+
+firebase.initializeApp({
+    apiKey: "AIzaSyCxRTkWjoToUoNsM8Rm6zPwiJBG_JCB4fo",
+    authDomain: "verycoolthanksforsharing.firebaseapp.com",
+    databaseURL: "https://verycoolthanksforsharing.firebaseio.com",
+    projectId: "verycoolthanksforsharing",
+    storageBucket: "verycoolthanksforsharing.appspot.com",
+    messagingSenderId: "78626384450",
+    appId: "1:78626384450:web:5c1a5470485502ab"
+});
