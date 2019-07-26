f2d38eacebd4c55dceb72323eaff62f88fc0a2ce
verycoolmathgames <52218872+verycoolmathgames@users.noreply.github.com>
2019-07-26 15:29:19 -0400

    Gitacora saved 3 changed files

--- public/main/points.js
+++ public/main/points.js
@@ -2,10 +2,6 @@
 
 import * as firebase from "firebase/app";
 import "firebase/app";
-import "firebase/auth";
-import "firebase/firestore";
-import "firebase/functions";
-import "firebase/database";
 
 import "firebase/auth";
 import "firebase/firestore";
