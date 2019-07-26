16a30c4f865558ec40cbe0cd47a950aab03b5514
verycoolmathgames <52218872+verycoolmathgames@users.noreply.github.com>
2019-07-26 15:10:01 -0400

    Gitacora saved 1 changed files

--- public/main/points.js
+++ public/main/points.js
@@ -34,7 +34,7 @@ totalPointsRef.onSnapshot(function (doc) {
     console.log("Current data: ", doc.data());
 });
 
-var memeUploadsRef = db.doc("leaderboards"/"totalPoints").collection("totalPoints").doc(user.displayName);
+var memeUploadsRef = db.doc("leaderboards"/"totalPoints"/"totalPoints").doc(user.displayName);
 
 totalPointsRef.get().then(function (doc) {
     if (doc.exists) {
