75f1aa510017f96da502e71cb6d8706dd106edf9
verycoolmathgames <52218872+verycoolmathgames@users.noreply.github.com>
2019-07-26 15:33:11 -0400

    Gitacora saved 1 changed files

--- public/main/points.js
+++ public/main/points.js
@@ -1,9 +1,5 @@
 /* eslint-disable no-unused-vars */
 
-import firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/firestore";
-
 window.document.addEventListener("DOMContentLoaded", event => {
 
     const firebase = require(firebase());
