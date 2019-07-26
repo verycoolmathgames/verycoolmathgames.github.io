f1188df717cf5f00ac5aac847ff03b5d30e637c1
verycoolmathgames <52218872+verycoolmathgames@users.noreply.github.com>
2019-07-26 15:10:52 -0400

    Gitacora saved 1 changed files

--- public/main/points.js
+++ public/main/points.js
@@ -17,7 +17,7 @@ firebase.initializeApp({
 
 var user = firebase.auth().currentUser;
 var db = firebase.firestore();
-var totalPointsRef = db."'leaderboards'/'totalPoints'/'totalPoints'/user.displayName";
+var totalPointsRef = db."'leaderboards'/'totalPoints'/'totalPoints'/user.displayName");
 
 totalPointsRef.get().then(function (doc) {
     if (doc.exists) {
