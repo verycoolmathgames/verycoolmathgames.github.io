a2eee79096ff469ce81cd80b22ed03e6e7cd43e8
verycoolmathgames <52218872+verycoolmathgames@users.noreply.github.com>
2019-07-26 14:58:34 -0400

    Gitacora saved 1 changed files

--- public/main/points.js
+++ public/main/points.js
@@ -19,6 +19,8 @@ var user = firebase.auth().currentUser;
 var db = firebase.firestore();
 var totalPoints = db.collection("leaderboards").doc("totalPoints").collection("totalPoints").doc(user.displayName);
 
+
+
 function onloadPoints() {
 
 };
