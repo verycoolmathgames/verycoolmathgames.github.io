4272776d7e264dbe820a3f90f2d6770fa35f202a
verycoolmathgames <52218872+verycoolmathgames@users.noreply.github.com>
2019-07-26 15:33:01 -0400

    Gitacora saved 1 changed files

--- public/main/points.js
+++ public/main/points.js
@@ -4,6 +4,21 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 
+window.document.addEventListener("DOMContentLoaded", event => {
+
+    const firebase = require(firebase());
+    const app = firebase.app();
+    const db = firebase.firestore();
+    const chatHistory = db.collection("website_chat");
+
+    chatHistory.onSnapshot(doc => {
+        const chatData = doc.data();
+        var name = document.createTextNode(chatData.displayName + `<br>`);
+        var text = document.createTextNode(chatData.text + `<br>`);
+    })
+
+});
+
 firebase.initializeApp({
     apiKey: "AIzaSyCxRTkWjoToUoNsM8Rm6zPwiJBG_JCB4fo",
     authDomain: "verycoolthanksforsharing.firebaseapp.com",
